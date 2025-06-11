4b7a1af4a684057e3d3402b9d9705d5cacaff3bb
baikalrs <baikalserbia@gmail.com>
2025-06-11 14:04:20 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,35 @@
+
+import logging
+from telegram import Update, ReplyKeyboardMarkup
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, MessageHandler, filters
+import os
+
+logging.basicConfig(level=logging.INFO)
+TOKEN = os.getenv("BOT_TOKEN")
+
+# Старт
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user = update.effective_user
+    await update.message.reply_text(
+        f"Привет, {user.first_name}! Добро пожаловать в Baikal 🥟.\n\nВыбери категорию товара:",
+        reply_markup=ReplyKeyboardMarkup([
+            ["Пельмени", "Вареники"],
+            ["Сырники и блинчики", "Тортики"],
+            ["Ваша корзина"]
+        ], resize_keyboard=True)
+    )
+
+# Обработка текста
+async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    text = update.message.text
+    await update.message.reply_text(f"Вы выбрали: {text}. (Здесь будет выбор товаров).")
+
+# Запуск
+def main():
+    app = ApplicationBuilder().token(TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
+    app.run_polling()
+
+if __name__ == "__main__":
+    main()
